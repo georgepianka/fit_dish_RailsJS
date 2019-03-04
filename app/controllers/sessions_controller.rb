b097c41e4f2b6b4964269bdf8c68cd99539948ba
NotoriousCottonball <gpianka1@gmail.com>
2019-03-03 20:53:26 -0800

    Added SessionsController and Root Path/View.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if auth_hash = request.env["omniauth.auth"]
+      @user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+     @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        redirect_to login_path
+        flash[:notice] = "Error: Please Login"
+      end
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to '/'
+  end
+
+end
