64f9704a42dc28dabbd772702079a2fa1d9dfddd
NotoriousCottonball <gpianka1@gmail.com>
2019-03-15 23:13:32 -0700

    Added ingredient_attributes= writer to RecipeIngredients for Nested Attributes

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,4 +3,13 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :ingredient
 
   validates :quantity, presence: true
+
+  def ingredient_attributes=(ingredient_attributes)
+    ingredient_attributes.values.each do |val|
+      if attr.present?
+        self.ingredient = Ingredient.where(name: val).first_or_create
+      end
+    end
+  end
+
 end
