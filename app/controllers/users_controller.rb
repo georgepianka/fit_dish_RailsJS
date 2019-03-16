3992c8986af91f4eac056783ea639d99648b00d3
NotoriousCottonball <gpianka1@gmail.com>
2019-03-16 03:45:07 -0700

    Added Sign-Up UsersController actions and Users Views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def index
+    redirect_to new_user_path
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash.now.notice = "Error. Please try again #{@user.errors.full_messages.to_sentence}."
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+end
