8a5164d2d6c3435c20adfc3f6b2fa1db0c4cb867
NotoriousCottonball <gpianka1@gmail.com>
2019-03-15 23:57:43 -0700

    Passed optional:true to RecipeIngredient belongs_to association, allowing it to be created without a parent object

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,6 +1,6 @@
 class RecipeIngredient < ApplicationRecord
-  belongs_to :recipe
-  belongs_to :ingredient
+  belongs_to :recipe, optional:true
+  belongs_to :ingredient, optional:true
 
   validates :quantity, presence: true
 
