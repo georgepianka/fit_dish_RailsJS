0b8a43b7e962c8c0b4bdafa3a918bc2723eaf3f6
NotoriousCottonball <gpianka1@gmail.com>
2019-03-23 10:57:30 -0700

    Added Grocery List to User Show Template and Dishes Index

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,7 +3,7 @@ class DishesController < ApplicationController
   before_action :authorized?
 
     def index
-      @dishes = current_user.ingredient_lists.order( created_at: :desc)
+      @dishes = current_user.dishes.order( created_at: :desc)
     end
 
     def create
@@ -18,24 +18,11 @@ class DishesController < ApplicationController
       end
     end
 
-      def edit
-        @dish = Dish.find(params[:id])
-      end
-
-      def update
-        @dish = Dish.find(params[:id])
-          @dish.update(note: params[:dish][:note])
-        if @dish.save
-          redirect_to user_dishs_path
-        else
-          render :edit
-        end
-      end
-
       def destroy
           @dish = Dish.find(params[:id])
           @dish.destroy
-          redirect_to user_dishs_path
+          flash[:primary] = "Dish Removed!"
+          redirect_to user_dishes_path(current_user)
       end
 
       private
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,8 +3,4 @@ class Dish < ApplicationRecord
   belongs_to :recipe
 
 
-  def list_ingredients
-      self.recipe.recipe_ingredients.map{|ing| ing.ingredient.name}
-  end
-
 end
