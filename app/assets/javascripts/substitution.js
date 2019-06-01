73ca9ae15b421d9d17ad4172dc0ac985f688f84b
NotoriousCottonball <gpianka1@gmail.com>
2019-05-31 23:49:21 -0700

    Cleans-Up substitution.js, Organizing Functions Inside Class Model Construct: Clean, Easy-To-Follow, NameSpaced

--- app/assets/javascripts/substitution.js
+++ app/assets/javascripts/substitution.js
@@ -1,37 +1,37 @@
 class Substitution {
  constructor(obj) {
-   this.id = obj.id
-   this.recipeID = obj.recipe.id
-   this.quantity = obj.quantity
-   this.description = obj.description
-   this.ingredientName = obj.ingredient.name
-   this.replacedIngredientName = obj.recipe_ingredient.ingredient.name
-   this.replacedIngredientQuantity = obj.recipe_ingredient.quantity
+     this.id = obj.id
+     this.recipeID = obj.recipe.id
+     this.quantity = obj.quantity
+     this.description = obj.description
+     this.ingredientName = obj.ingredient.name
+     this.replacedIngredientName = obj.recipe_ingredient.ingredient.name
+     this.replacedIngredientQuantity = obj.recipe_ingredient.quantity
  }
 
  renderSubstitution() {
-   return `
-    <tr>
-     <td><h5><i><li>${this.quantity}</i><span class="float-right ml-3"><strong><b>${this.ingredientName}</span></li></strong></b></td></h5>
-     <td>
-       <h6>
-         <div class="center"><i class="fas fa-exchange-alt"></i></div>
-         <div class="center"> &nbsp; Replacing &nbsp; </div>
-       </h6>
-   </td>
-     <td><h5 class="replaced-ingredient"><i>${this.replacedIngredientQuantity}</i><span class="float-right ml-2"><strong><b>${this.replacedIngredientName}</span></strong></b></td></h5>
-     <td>
-       <h5 class="replaced-ingredient"><a class="float-right m-auto btn btn-dark border btn-sm border-muted" data-confirm="Are you sure?" rel="nofollow" data-method="delete" href="/recipes/${this.recipeID}/substitutions/${this.id}">Delete</a></h5>
+     return `
+      <tr>
+       <td><h5><i><li>${this.quantity}</i><span class="float-right ml-3"><strong><b>${this.ingredientName}</span></li></strong></b></td></h5>
+       <td>
+         <h6>
+           <div class="center"><i class="fas fa-exchange-alt"></i></div>
+           <div class="center"> &nbsp; Replacing &nbsp; </div>
+         </h6>
      </td>
-   </tr>
-   <tr>
-     <td colspan="4"><h6><b>Description: </b></h6>
-       <h6>${this.description}</h6>
-     </td>
-   </tr>
-   <tr><td class="bg-dark p-1" colspan="4"></td>
-   </tr>
-  `
+       <td><h5 class="replaced-ingredient"><i>${this.replacedIngredientQuantity}</i><span class="float-right ml-2"><strong><b>${this.replacedIngredientName}</span></strong></b></td></h5>
+       <td>
+         <h5 class="replaced-ingredient"><a class="float-right m-auto btn btn-dark border btn-sm border-muted" data-confirm="Are you sure?" rel="nofollow" data-method="delete" href="/recipes/${this.recipeID}/substitutions/${this.id}">Delete</a></h5>
+       </td>
+     </tr>
+     <tr>
+       <td colspan="4"><h6><b>Description: </b></h6>
+         <h6>${this.description}</h6>
+       </td>
+     </tr>
+     <tr><td class="bg-dark p-1" colspan="4"></td>
+     </tr>
+    `
  }
 
  static getSubstitutionForm() {
@@ -70,13 +70,13 @@ class Substitution {
  }
 
  static displayErrors(show_errors) {
-       console.log(show_errors)
+     console.log(show_errors)
 
-       const errorMessages = `<div id="error-messages" class="text-center text-danger m-2"><div class="center alert alert-danger m-3">Please Try Again!</div>${show_errors}</div>`;
+     const errorMessages = `<div id="error-messages" class="text-center text-danger m-2"><div class="center alert alert-danger m-3">Please Try Again!</div>${show_errors}</div>`;
 
-       $("div#new-substitution-form").children("#error-messages").remove();
-       $("div#new-substitution-form").prepend(errorMessages);
-       $('form input[value="Recommend Substitution!"]').prop("disabled", false);
+     $("div#new-substitution-form").children("#error-messages").remove();
+     $("div#new-substitution-form").prepend(errorMessages);
+     $('form input[value="Recommend Substitution!"]').prop("disabled", false);
  }
 
  static updateSubstitutions(data) {
@@ -92,5 +92,5 @@ class Substitution {
 }
 
 $(document).on('turbolinks:load', function() {
-  Substitution.ready();
+    Substitution.ready();
 })
