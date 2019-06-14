04a7c77881cab532d9b548c79fd9d2ae229a9068
NotoriousCottonball <gpianka1@gmail.com>
2019-06-13 22:15:30 -0700

    Fix Recipe Page link href generated in each li of Recipe Index by formatting Recipe Serializer to include an attribute for Current User instead of User_Id

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -5,7 +5,7 @@ class Recipe {
      this.description = obj.description
      this.instructions = obj.instructions
      this.like_count = obj.like_count
-     this.user_id = obj.user_id
+     this.current_user_id = obj.current_user_id
      this.recipe_ingredients = obj.recipe_ingredients
 
      }
@@ -15,7 +15,7 @@ class Recipe {
       <li id="recipe-index-item" class="list-group-item hvr-grow px-0">
         <span class="float-left align-middle mt-1"><span class="btn btn-sm"><i class="fas fa-chevron-circle-down"></i></span>${this.name}</span>
         <a class="btn btn-outline-dark btn-sm float-right" href="#"><span><i class="fas fa-plus"></i></span></a>
-        <a id="add-recipe-to-dishes" class="btn btn-sm btn-primary border border-muted mr-2 float-right" href="/users/${this.user_id}/recipes/${this.id}">Recipe Page</a>
+        <a id="add-recipe-to-dishes" class="btn btn-sm btn-primary border border-muted mr-2 float-right" href="/users/${this.current_user_id}/recipes/${this.id}">Recipe Page</a>
         <span class="float-right mr-2 mt-1">
         ${this.like_count > 0 ? this.like_count : ''}&nbsp;<i class="fas fa-grin-hearts"></i>
         </span>
@@ -132,7 +132,8 @@ class Recipe {
 
   static displayRecipeInfo(recipe) {
     let recipe_info = new this(recipe)
-    console.log(JSON.parse(recipe))
+    console.log(recipe_info.name)
+
   }
 
   static ready() {
--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,12 +1,12 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :instructions, :like_count, :user_id
+  attributes :id, :name, :description, :instructions, :like_count, :current_user_id
   has_many :recipe_ingredients, serializer: RecipeIngredientSerializer
   def like_count
     object.likes.count
   end
 
-  def user_id
-    object.user.id
+  def current_user_id
+    current_user.id
   end
 
 end
