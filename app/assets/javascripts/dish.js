0be87770fe17b91153356e85355d5bd5e8783296
NotoriousCottonball <gpianka1@gmail.com>
2019-06-14 14:12:42 -0700

    Formats newDishHTML() prototype method to output added Dishes to user dish index

--- app/assets/javascripts/dish.js
+++ app/assets/javascripts/dish.js
@@ -1,19 +1,32 @@
 
 class Dish {
   constructor(obj) {
+    this.id = obj.id
     this.recipeID = obj.recipe.id
     this.name = obj.recipe.name
     this.likeCount = obj.recipe.like_count
     this.currentUserID = obj.recipe.current_user_id
   }
 
-  newDishHTML() {}
+  newDishHTML() {
+    return `
+    <li class="list-group-item">
+      <span class="align-middle">${this.name}</span>
+      <span class="align-middle ml-2">${this.likeCount > 0 ? this.likeCount : ''}&nbsp;<i class="fas fa-grin-hearts"></i></span>
+      <span class="float-right">
+      <a class="btn btn-sm btn-primary border border-muted align-middle ml-2" href="/users/${this.currentUserID}/recipes/${this.recipeID}">Recipe Page</a>
+      <a class="btn btn-dark border btn-sm border-muted align-middle" data-confirm="Are you sure?" rel="nofollow" data-method="delete" href="/users/${this.currentUserID}/dishes/${this.id}">Delete Dish</a>
+      </span>
+    </li>
+    `
+  }
 
   static addToDishes() {
       $("div#dishes-recipe-index").on('click', "a.add-recipe-to-dishes", function(e) {
         e.preventDefault();
         e.stopPropagation();
         console.log($(this).data("current-user-id"))
+
         let dishData = {dish:{user_id:$(this).data("current-user-id"),recipe_id:$(this).data("recipe-id")}}
         console.log(dishData)
 
@@ -34,8 +47,11 @@ class Dish {
 
   static addNewDish(dish) {
     console.log(dish)
-    $("div#user-dishes-index")
+    !$("div#user-dishes-index > h3").length || $("div#user-dishes-index > h3").empty();
+    $("div#user-dishes-index > ul").append(new this(dish).newDishHTML());
   }
+
+
 
   static ready() {
     this.addToDishes();
